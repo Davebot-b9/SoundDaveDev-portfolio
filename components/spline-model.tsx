3869bb86320fde1264367e2ba8bb74c20ec89c35
Oscar David Morelos Reyes <odmr345@gmail.com>
2024-08-22 13:26:04 -0600

    changes optimization responsive model

--- components/spline-model.tsx
+++ components/spline-model.tsx
@@ -1,12 +1,17 @@
-import Spline from '@splinetool/react-spline/next';
+'use client';
+// import Spline from '@splinetool/react-spline/next';
+import React, { Suspense } from 'react';
 
-const Model = () =>{
-    return(
-        <div>
-            <Spline scene='https://prod.spline.design/D5GSsOQ8UJ0xnQdP/scene.splinecode'/>
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
+
+const Model = () => {
+    return (
+        <div className='flex justify-center items-center w-full h-full md:w-[70%] md:h-[80vh] max-w-[800px] max-h-[600px]'>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Spline className='w-full h-full object-contain' scene='https://prod.spline.design/D5GSsOQ8UJ0xnQdP/scene.splinecode' />
+            </Suspense>
         </div>
     );
-}
-
+};
 
 export default Model;
